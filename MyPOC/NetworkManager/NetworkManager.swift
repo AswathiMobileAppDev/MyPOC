70c6ab0c9f4adb3985af51b1f79c0170c6a108c1
Aswathi <mailtoaswathibaskaran@gmail.com>
2022-09-23 15:40:25 +0530

    mypoc

--- MyPOC/NetworkManager/NetworkManager.swift
+++ MyPOC/NetworkManager/NetworkManager.swift
@@ -11,13 +11,13 @@ class NetworkManager {
     
     static let shared = NetworkManager()
     
-    func makeRequest(request: URLRequest, handler: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+    func makeRequest(request: URLRequest, handler: @escaping (Result<(Data, URLResponse), Error>) -> Void) {
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 DispatchQueue.main.async {
                     handler(.failure(error))
                 }
-            } else if let data = data, let urlResponse = response as? HTTPURLResponse {
+            } else if let data = data, let urlResponse = response {
                 DispatchQueue.main.async {
                     handler(.success((data, urlResponse)))
                 }
